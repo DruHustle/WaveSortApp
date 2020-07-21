ab6f30dde7bb14cb3054da05e8b542d3bced7942
Andrew Gotora <andrewgotora@yahoo.com>
2020-07-21 10:41:46 +0200

    Wave Sort First Commit

---
+++ WaveSortApp/FormGUI.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace WaveSortApp
+{
+    partial class FormGUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormGUI));
+            this.textFilePath = new System.Windows.Forms.TextBox();
+            this.loadCSVFilesButton = new System.Windows.Forms.Button();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.downloadCSVFileButton = new System.Windows.Forms.Button();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.amplitudeChart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.sortVoltageAmplitude = new System.Windows.Forms.Button();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.amplitudeChart)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textFilePath
+            // 
+            this.textFilePath.Location = new System.Drawing.Point(14, 377);
+            this.textFilePath.Name = "textFilePath";
+            this.textFilePath.Size = new System.Drawing.Size(311, 20);
+            this.textFilePath.TabIndex = 0;
+            // 
+            // loadCSVFilesButton
+            // 
+            this.loadCSVFilesButton.Location = new System.Drawing.Point(341, 374);
+            this.loadCSVFilesButton.Name = "loadCSVFilesButton";
+            this.loadCSVFilesButton.Size = new System.Drawing.Size(103, 23);
+            this.loadCSVFilesButton.TabIndex = 7;
+            this.loadCSVFilesButton.Text = "Load CSV Files";
+            this.loadCSVFilesButton.UseVisualStyleBackColor = true;
+            this.loadCSVFilesButton.Click += new System.EventHandler(this.LoadCSVFilesButton_Click);
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(14, 415);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(772, 23);
+            this.progressBar.TabIndex = 2;
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(698, 374);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(86, 23);
+            this.exitButton.TabIndex = 7;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.ExitButton_Click);
+            // 
+            // downloadCSVFileButton
+            // 
+            this.downloadCSVFileButton.Location = new System.Drawing.Point(579, 374);
+            this.downloadCSVFileButton.Name = "downloadCSVFileButton";
+            this.downloadCSVFileButton.Size = new System.Drawing.Size(103, 23);
+            this.downloadCSVFileButton.TabIndex = 5;
+            this.downloadCSVFileButton.Text = "Download CSV";
+            this.downloadCSVFileButton.UseVisualStyleBackColor = true;
+            this.downloadCSVFileButton.Click += new System.EventHandler(this.DownloadCSVFile_Click);
+            // 
+            // amplitudeChart
+            // 
+            this.amplitudeChart.BorderlineColor = System.Drawing.Color.Black;
+            this.amplitudeChart.BorderlineDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.Solid;
+            this.amplitudeChart.BorderSkin.BackColor = System.Drawing.SystemColors.GrayText;
+            this.amplitudeChart.BorderSkin.BorderDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.Solid;
+            this.amplitudeChart.BorderSkin.BorderWidth = 2;
+            chartArea1.Name = "ChartArea1";
+            this.amplitudeChart.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.amplitudeChart.Legends.Add(legend1);
+            this.amplitudeChart.Location = new System.Drawing.Point(14, 12);
+            this.amplitudeChart.Name = "amplitudeChart";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Volts";
+            this.amplitudeChart.Series.Add(series1);
+            this.amplitudeChart.Size = new System.Drawing.Size(772, 341);
+            this.amplitudeChart.TabIndex = 1;
+            this.amplitudeChart.Text = "Amplitude Chart";
+            // 
+            // sortVoltageAmplitude
+            // 
+            this.sortVoltageAmplitude.Location = new System.Drawing.Point(460, 374);
+            this.sortVoltageAmplitude.Name = "sortVoltageAmplitude";
+            this.sortVoltageAmplitude.Size = new System.Drawing.Size(103, 23);
+            this.sortVoltageAmplitude.TabIndex = 8;
+            this.sortVoltageAmplitude.Text = "Sort Amplitude";
+            this.sortVoltageAmplitude.UseVisualStyleBackColor = true;
+            this.sortVoltageAmplitude.Click += new System.EventHandler(this.SortVoltageAmplitude_Click);
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Location = new System.Drawing.Point(14, 12);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.Size = new System.Drawing.Size(772, 341);
+            this.dataGridView.TabIndex = 9;
+            // 
+            // FormGUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView);
+            this.Controls.Add(this.sortVoltageAmplitude);
+            this.Controls.Add(this.amplitudeChart);
+            this.Controls.Add(this.downloadCSVFileButton);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.loadCSVFilesButton);
+            this.Controls.Add(this.textFilePath);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "FormGUI";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FormGUI";
+            this.Load += new System.EventHandler(this.FormGUI_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.amplitudeChart)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textFilePath;
+        private System.Windows.Forms.Button loadCSVFilesButton;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Button downloadCSVFileButton;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.DataVisualization.Charting.Chart amplitudeChart;
+        private System.Windows.Forms.Button sortVoltageAmplitude;
+        private System.Windows.Forms.DataGridView dataGridView;
+    }
+}
+
---
+++ WaveSortApp/FormGUI.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace WaveSortApp
+{
+    public partial class FormGUI : Form
+    {
+        private readonly Dictionary<string, double> fileNameAmplitudeDictionary = new Dictionary<string, double>();
+        private readonly Dictionary<string, double> fileNameAmplitudeSortedDictionary = new Dictionary<string, double>();
+        private string[] fileNames;
+
+        public FormGUI()
+        {
+            InitializeComponent();
+        }
+
+        private void FormGUI_Load(object sender, EventArgs e)
+        {
+            //chart title  
+            amplitudeChart.Titles.Add("Volt Amplitude Per File");
+
+            //Clear workspace
+            Reset();
+        }
+
+        private void LoadCSVFilesButton_Click(object sender, EventArgs e)
+        {
+            //Clear workspace
+            Reset();
+
+            DialogResult dialogResult = openFileDialog.ShowDialog();
+
+            if (dialogResult == DialogResult.OK)
+            {
+                fileNames = openFileDialog.FileNames;
+
+                //Update textbox text
+                PrintTextBox("Loading.....");
+
+                if (fileNames != null)
+                {
+                    //Set progress bar values
+                    progressBar.Value = 0;
+                    progressBar.Minimum = 0;
+                    progressBar.Maximum = 100;
+
+                    //Clear dictionary
+                    fileNameAmplitudeDictionary.Clear();
+
+                    //Extract amplitude and save to Dictionary
+                    foreach (string fileName in fileNames)
+                    {
+                        try
+                        {
+                            //Extract amplitude from file
+                            double amplitude = GetAmplitude(fileName);
+
+                            //Add filename and amplitude to a dictionary
+                            AddDataToDictionary(fileName, amplitude);
+
+                            //Update progress bar
+                            progressBar.Value = (Array.IndexOf(openFileDialog.FileNames, fileName) + 1) * 100 / openFileDialog.FileNames.Length;
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show(ex.Message, "GUI Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                        }
+                    }
+                }
+                else
+                {
+                    //Update textbox text
+                    PrintTextBox("Loading Error!");
+                }
+
+                //Update textbox text
+                PrintTextBox("Loading Complete.");
+
+                //Enable sort button
+                sortVoltageAmplitude.Enabled = true;
+            }
+        }
+
+        private void SortVoltageAmplitude_Click(object sender, EventArgs e)
+        {
+            //Set progress bar values and loop tracker
+            int loopTracker = 0;
+            progressBar.Value = 0;
+            progressBar.Minimum = 0;
+            progressBar.Maximum = 100;
+
+            //Clear dictionary
+            fileNameAmplitudeSortedDictionary.Clear();
+
+            //Clear Chart
+            amplitudeChart.Series["Volts"].Points.Clear();
+
+            //Update textbox text
+            PrintTextBox("Sorting....");
+
+            if (fileNameAmplitudeDictionary != null)
+            {
+                foreach (KeyValuePair<string, double> entry in fileNameAmplitudeDictionary.OrderBy(key => key.Value))
+                {
+                    //Add filename and amplitude to a dictionary
+                    AddDataToSortedDictionary(entry.Key, entry.Value );
+
+                   //Chart visualization
+                    AddDataToChart(entry.Key, entry.Value);
+
+                    //Update progress bar
+                    progressBar.Value = (loopTracker + 1) * 100 / fileNameAmplitudeDictionary.Count;
+                    loopTracker++;
+                }
+            }
+            else
+            {
+                //Update textbox text
+                PrintTextBox("Sorting Error!");
+            }
+
+            //Show chart
+            amplitudeChart.Visible = true;
+            dataGridView.Visible = false;
+
+            //Enable download button
+            downloadCSVFileButton.Enabled = true;
+
+            //Update textbox text
+            PrintTextBox("Sorting Complete.");
+        }
+
+        private void DownloadCSVFile_Click(object sender, EventArgs e)
+        {
+            if (fileNameAmplitudeDictionary!= null)
+            {
+                string csv = string.Join(Environment.NewLine, fileNameAmplitudeDictionary.Select(d => $"{d.Key},{d.Value},"));
+                string fileFolder = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+                string fileSavePath = fileFolder + "/SortedWave.csv";
+                File.WriteAllText(fileSavePath, csv);
+
+                //Update textbox text
+                PrintTextBox("Download complete. Please find file in the \"Documents\"folder.");
+            }
+            else
+            {
+                //Update textbox text
+                PrintTextBox("Download Error!");
+            }
+        }
+
+        private void Reset()
+        {
+            //Display datagridview to create a nice canvas over the UI.
+            dataGridView.Visible = true;
+
+            // Set dialog box filter, check path and check file settings.
+            openFileDialog.Filter = "CSV Files | *.csv";
+            openFileDialog.CheckFileExists = true;
+            openFileDialog.CheckPathExists = true;
+
+            //Set the Dialog box to multi-select.
+            openFileDialog.Multiselect = true;
+
+            //Disable selected buttons
+            downloadCSVFileButton.Enabled = false;
+            sortVoltageAmplitude.Enabled = false;
+
+            //Textbox initial text
+            textFilePath.Text = "No Files Added.";
+
+            //Hide selected items
+            textFilePath.Enabled = false;
+            amplitudeChart.Visible = false;
+        }
+
+        private double GetAmplitude(string fileName)
+        {
+            //Read text all text in file
+            string[] lines = File.ReadAllLines(fileName);
+
+            //Get header labels
+            string[] headerLabels = lines[0].Split(',');
+            int voltsIndex = Array.IndexOf(headerLabels, "Volts");
+
+            //List to store volts values
+            List<double> volts = new List<double>();
+
+            if (lines.Length > 0)
+            {
+                for (int i = 1; i < lines.Length; i++)
+                {
+                    //Obtaining string in cells of the row and adding volts to the volt list
+                    string[] dataWords = lines[i].Split(',');
+                    volts.Add(Convert.ToDouble(dataWords[voltsIndex]));
+                }
+            }
+
+            return (double)volts.Max();
+        }
+
+        private void AddDataToDictionary(string fileName, double amplitude)
+        {
+            //Add values to dictionary
+            fileNameAmplitudeDictionary.Add(Path.GetFileName(fileName), amplitude);
+        }
+
+        private void AddDataToSortedDictionary(string key, double value)
+        {
+            //Add values to sorted dictionary
+            fileNameAmplitudeSortedDictionary.Add(key, value);
+        }
+
+        private void AddDataToChart(string key, double value)
+        {
+            //AddXY value in chart in series  
+            amplitudeChart.Series["Volts"].Points.AddXY(key, value);
+        }
+
+        private void PrintTextBox(string text)
+        {
+            //Update textbox text
+            textFilePath.Text = text;
+            Application.DoEvents();
+            Thread.Sleep(100);
+        }
+
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            DialogResult ans = MessageBox.Show("Are you sure you want to exit?", "GUI Message", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if (ans == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+    }
+}
